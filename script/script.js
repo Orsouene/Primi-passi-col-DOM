c73cacf54ee614cd6a943fda1c57c2e50f4f0dfb
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-07 16:28:58 +0100

    Fine esercizio con il bonus

--- script/script.js
+++ script/script.js
@@ -2,10 +2,20 @@
 const lampadinaSpenta = document.getElementById("spenta");
 const buttoneAccendere = document.getElementById("accendere");
 // la function per scambiare l'img al momento del click
-function accensione() {
-  if (lampadinaSpenta.src.includes("white_lamp.png")) {
+function accendiSpegni() {
+  if (
+    lampadinaSpenta.src.includes("white_lamp.png") &&
+    (document.getElementById("accendere").innerHTML = "Accendi")
+  ) {
     lampadinaSpenta.src = "./img/yellow_lamp.png";
+    document.getElementById("accendere").innerHTML = "spegni";
+  } else if (
+    lampadinaSpenta.src.includes("yellow_lamp.png") &&
+    (document.getElementById("accendere").innerHTML = "spegni")
+  ) {
+    lampadinaSpenta.src = "./img/white_lamp.png";
+    document.getElementById("accendere").innerHTML = "Accendi";
   }
 }
 // applicare la function sull buttone  al momento del click
-buttoneAccendere.addEventListener("click", accensione);
+buttoneAccendere.addEventListener("click", accendiSpegni);
